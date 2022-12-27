3324e8f1086c39aeb1c096c5a57ba44b39ecdb77
MOON-TG <takeyou@kakao.com>
2022-12-27 19:07:17 +0900

    admin 포스트 추가 및 작성시간 자동설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length=30)
+  content = models.TextField()
+
+  created_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now=True)
+  #autor : 추후 예정
+
+  def __str__(self):
+    return f'{self.title}'
