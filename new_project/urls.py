a19910e733fb0f6666b2e3ab449a4bbb09e97fef
MOON-TG <takeyou@kakao.com>
2023-01-09 19:41:07 +0900

    다중 이미지 구현 전

--- new_project/urls.py
+++ new_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('agency/', include('blog.urls')),
     path('', include('singe_pages.urls')),
 ]
 
