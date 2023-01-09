3c7de388dbc5b32665ec3f2281ad5a0499de6e6f
MOON-TG <takeyou@kakao.com>
2023-01-09 19:57:53 +0900

    if문으로 다중 이미지 업로드 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
   content = models.TextField()
 
   head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+  head_image2 = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
 
   created_at = models.DateTimeField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
